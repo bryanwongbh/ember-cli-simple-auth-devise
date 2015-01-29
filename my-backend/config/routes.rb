2a0279820817d0c0c9567a50596a308215051095
bryanwongbh <bryanwongbh@gmail.com>
2015-01-29 10:50:57 +0000

    added admin_user to separate admin and normal user table

--- my-backend/config/routes.rb
+++ my-backend/config/routes.rb
@@ -1,6 +1,8 @@
 MyBackend::Application.routes.draw do
-  devise_for :users, ActiveAdmin::Devise.config
+  devise_for :users, controllers: { sessions: 'sessions' }
+  
+  devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  # devise_for :users, controllers: { sessions: 'sessions' }
+  
   # root to: "static#index"
 end
