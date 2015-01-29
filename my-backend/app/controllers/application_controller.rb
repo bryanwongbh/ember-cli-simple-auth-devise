6b9f209e516ae8f8c2675bed30d63b1c4b72c5f6
Bryan <bwbh87@gmail.com>
2015-01-29 14:13:24 +0800

    added activeadmin into this

--- my-backend/app/controllers/application_controller.rb
+++ my-backend/app/controllers/application_controller.rb
@@ -5,6 +5,19 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
  
+
+  def authenticate_admin_user!
+    authenticate_user! 
+    unless current_user.admin?
+      flash[:alert] = "This area is restricted to administrators only."
+      redirect_to root_path 
+    end
+  end
+ 
+  def current_admin_user
+    return nil if user_signed_in? && !current_user.admin?
+    current_user
+  end
   private
  
     def authenticate_user_from_token!
--- my-backend/config/routes.rb
+++ my-backend/config/routes.rb
@@ -1,4 +1,6 @@
 MyBackend::Application.routes.draw do
-  devise_for :users, controllers: { sessions: 'sessions' }
-  root to: "static#index"
+  devise_for :users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  # devise_for :users, controllers: { sessions: 'sessions' }
+  # root to: "static#index"
 end
